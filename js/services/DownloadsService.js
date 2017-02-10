05a6665287b6d99772530f52d971c81fb3096d8c
andradadrus <andrada.drus@gmail.com>
2017-02-10 13:31:06 +0200

    error solve

--- js/services/DownloadsService.js
+++ js/services/DownloadsService.js
@@ -50,45 +50,9 @@ angular.module($APP.name).factory('DownloadsService', [
                                                 );
                                             },
                                             false);
-
-
-                            if (ionic.Platform.isIPad() || ionic.Platform.isAndroid() || ionic.Platform.isIOS()) {
-
-                                if (cordova === undefined) {
-                                    cordova.file = {
-                                        dataDirectory: '///'
-                                    }
-                                }
-                                // CREATE
-                                $cordovaFile.createDir(cordova.file.dataDirectory, base64String, true)
-                                    .then(function(success) {
-                                        // success
-                                        console.log('dir created');
-                                        console.log(success);
-                                    }, function(error) {
-                                        // error
-                                        console.log(error);
-                                    });
-
-                                // Download
-                                var fileTransfer = new FileTransfer();
-                                var uri = encodeURI("$APP.server + '/pub/drawings/' + base64String"); // encodeURI("http://ionicframework.com/img/ionic-logo-blog.png");
-
-                                fileTransfer.download(
-                                    uri,
-                                    fileURL,
-                                    function(entry) {
-                                        console.log("download complete: " + entry.toURL());
-                                        $scope.Path = fileURL;
-                                    },
-                                    function(error) {
-                                        console.log("download error source " + error.source);
-                                        console.log("download error target " + error.target);
-                                        console.log("upload error code" + error.code);
-                                    }
-                                );
+                                    })
                             }
-                        })
+                        });
                     }).error(function(response) {
                         console.log(response);
                     });
