d9a04c39bbaa0349d4382a7f6a53aa43f114ea07
andradadrus <andrada.drus@gmail.com>
2017-02-10 12:46:38 +0200

    cordova reference added

--- js/services/DownloadsService.js
+++ js/services/DownloadsService.js
@@ -1,54 +1,61 @@
 angular.module($APP.name).factory('DownloadsService', [
     '$http',
     '$rootScope',
-    'Blob',
-    'FileSaver',
-  //  '$ionicPlatform',
-    function($http, $rootScope, FileSaver) {
+    '$ionicPlatform',
+    '$cordovaFile',
+    '$cordovaFileTransfer',
+    function($http, $rootScope, $ionicPlatform, $cordovaFile, $cordovaFileTransfer) {
         return {
             downloadPdf: function(base64String) {
                 return $http.get($APP.server + '/pub/drawings/' + base64String, {
                         responseType: 'blob'
                     })
                     .success(function(data, status, headers) {
-                      $ionicPlatform.ready(function() {
+                        $ionicPlatform.ready(function() {
+                            if (ionic.Platform.isIPad() || ionic.Platform.isAndroid() || ionic.Platform.isIOS()) {
+                                if (typeof cordova == 'undefined') {
+                                    cordova = {};
+                                    cordova.file = {
+                                        dataDirectory: '///'
+                                    }
+                                }
+                                console.log("cordova base dir: " + cordova.file.dataDirectory);
+                                //check space $cordovaFile.getFreeDiskSpace().then(function(success) { // success in kilobytes }, function ( error ) { // error });
+                                // CREATE
+                                //TODO: keep in indexdb
+                                $cordovaFile.createDir(cordova.file.dataDirectory, 'ds-downloads', true)
+                                    .then(function(success) {
+                                        // success
+                                        console.log('dir created');
+                                        console.log(success);
+                                        // Download
+                                        document.addEventListener(
+                                            "deviceready",
+                                            function() {
+                                                var fileTransfer = new FileTransfer();
+                                                var uri = encodeURI("$APP.server + '/pub/drawings/' + base64String"); // encodeURI("http://ionicframework.com/img/ionic-logo-blog.png");
 
-                          if(ionic.Platform.isIPad() || ionic.Platform.isAndroid() || ionic.Platform.isIOS()){
-
-                                fileTransferDir = cordova.file.dataDirectory;
-                                var fileURL = fileTransferDir + 'test/test.jpg';
-
-                              // CREATE
-                                  $cordovaFile.createDir(fileTransferDir, "test", false)
-                                    .then(function (success) {
-                                      // success
-                                      console.log('dir created');
-                                      console.log(success);
-                                    }, function (error) {
-                                      // error
-                                      console.log(error);
+                                                fileTransfer.download(
+                                                    uri,
+                                                    fileURL,
+                                                    function(entry) {
+                                                        console.log("download complete: " + entry.toURL());
+                                                        $scope.Path = fileURL;
+                                                    },
+                                                    function(error) {
+                                                        console.log("download error source " + error.source);
+                                                        console.log("download error target " + error.target);
+                                                        console.log("upload error code" + error.code);
+                                                    }
+                                                );
+                                            },
+                                            false);
+                                    }, function(error) {
+                                        // error
+                                        console.log(error);
                                     });
-
-                              // Download
-
-                              var fileTransfer = new FileTransfer();
-                              var uri = encodeURI("http://ionicframework.com/img/ionic-logo-blog.png");
-
-                              fileTransfer.download(
-                                  uri,
-                                  fileURL,
-                                  function(entry) {
-                                      console.log("download complete: " + entry.toURL());
-                                      $scope.Path=fileURL;
-                                  },
-                                  function(error) {
-                                      console.log("download error source " + error.source);
-                                      console.log("download error target " + error.target);
-                                      console.log("upload error code" + error.code);
-                                  }
-                              );
-                          }
-                      })
+                            }
+                        })
                     }).error(function(response) {
                         console.log(response);
                     });
